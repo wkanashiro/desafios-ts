5f3a6c539866e11bf283ef1cecb9e8a035f9539c
Wesley <wkanashiro@icloud.com>
2022-08-04 17:11:33 -0400

    Atualizado desafio 1 e desafio 2

--- desafios/desafio1.ts
+++ desafios/desafio1.ts
@@ -1,5 +1,6 @@
 // Como podemos rodar isso em um arquivo .ts sem causar erros? 
 
-let employee = {};
-employee.code = 10;
-employee.name = "John";
+const employee = {
+    code: 10,
+    name: "John"
+}
--- desafios/desafio2.ts
+++ desafios/desafio2.ts
@@ -1,23 +1,36 @@
 // Como podemos melhorar o esse código usando TS? 
 
-let pessoa1 = {};
-pessoa1.nome = "maria";
-pessoa1.idade = 29;
-pessoa1.profissao = "atriz"
+enum Cargo {
+    Atriz,
+    Padeiro
+}
 
-let pessoa2 = {}
-pessoa2.nome = "roberto";
-pessoa2.idade = 19;
-pessoa2.profissao = "Padeiro";
+type Pessoa = {
+    nome: string,
+    idade: number,
+    profissao: Cargo
+}
 
-let pessoa3 = {
-    nome: "laura",
-    idade: "32",
-    profissao: "Atriz"
+let pessoa1: Pessoa = {
+    nome: "maria",
+    idade: 29,
+    profissao: Cargo.Atriz
 };
 
-let pessoa4 = {
-    nome = "carlos",
-    idade = 19,
-    profissao = "padeiro"
+let pessoa2: Pessoa = {
+    nome: "roberto",
+    idade: 19,
+    profissao: Cargo.Padeiro
+}
+
+let pessoa3: Pessoa = {
+    nome: "laura",
+    idade: 32,
+    profissao: Cargo.Atriz
+};
+
+let pessoa4: Pessoa = {
+    nome: "carlos",
+    idade: 19,
+    profissao: Cargo.Padeiro
 }
